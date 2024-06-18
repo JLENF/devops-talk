6afb966a8f33ef4ce119bf91997bf90d3311106c
Jairo Lenfers <jairolenfers@gmail.com>
2024-06-18 19:32:11 -0300

    teste commit2

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_users():
 @app.route('/')
 def hello():
     hostname = socket.gethostname()
-    return f'Hello from Kubernetes Pod: {hostname}'
+    return f'Hello from Kubernetes Pod : {hostname}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
