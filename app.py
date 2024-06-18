65f4c2d8ef73e062416a11908d1d10f3d1af41f7
Jairo Lenfers <jairolenfers@gmail.com>
2024-06-18 19:39:04 -0300

    fix do erro

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_users():
 @app.route('/')
 def hello():
     hostname = socket.gethostname()
-    return f'Hello from Pod: {hostname}
+    return f'Hello from Pod: {hostname}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
