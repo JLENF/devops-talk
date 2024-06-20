3db550728cb2271e7a971a7d26ae5d06f7617860
Jairo Lenfers <jairolenfers@gmail.com>
2024-06-20 19:50:05 -0300

    release v1.0.6

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_users():
 @app.route('/')
 def hello():
     hostname = socket.gethostname()
-    return f'Hello from Pod (hostname): {hostname}'
+    return f'v1.0.6 - Hello from Pod: {hostname}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
