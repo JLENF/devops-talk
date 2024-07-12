9517fbb279b74efe8cad923e730fa39b4f508e6d
Jairo Lenfers <jairolenfers@gmail.com>
2024-07-12 16:39:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_users():
 @app.route('/')
 def hello():
     hostname = socket.gethostname()
-    return f'v1.0.9 - Hello from Pod: {hostname}'
+    return f'v1.0.10 - Hello from Pod -> {hostname}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
